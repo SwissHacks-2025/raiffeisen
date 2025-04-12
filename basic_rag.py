4dd8222ced8d73fe511be7a58f0f0dc6974f5793
johndoeteddy <johndoeteddy1@gmail.com>
2025-04-12 15:37:37 +0200

    added vectorDB for biases

--- basic_rag.py
+++ basic_rag.py
@@ -1,17 +1,28 @@
 from langchain_community.document_loaders import Docx2txtLoader
 from langchain_text_splitters import MarkdownHeaderTextSplitter
 
+from langchain_openai import AzureOpenAI
+
+from langchain_openai import AzureOpenAIEmbeddings
+
+from langchain_community.document_loaders import CSVLoader
+from langchain.vectorstores import Chroma
+import os
+
 import hydra
 from hydra.utils import get_original_cwd, to_absolute_path, instantiate, get_method
 
+from utils import get_client
+from dotenv import load_dotenv
 
 
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="basic_rag_config")
 def main(config):
+    load_dotenv()
 
-    # Load and split data
+    ### Load and split data ###
     loader = Docx2txtLoader(config.raiffeisen_products_path)
     data = loader.load()
     full_text = data[0].page_content
@@ -22,6 +33,30 @@ def main(config):
 
     markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
     md_header_splits = markdown_splitter.split_text(full_text)
+
+
+    # get embeddings
+    embeddings = AzureOpenAIEmbeddings(
+        model=config.embeddings.model_name,
+        azure_endpoint=config.azure_endpoint,
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        api_version=config.api_version,
+    )
+
+    # load bias and build vectordb
+    bias_loader = CSVLoader(config.bias_path)
+    bias_docs = bias_loader.load()
+    print(f"Loaded {len(bias_docs)} bias documents")
+    print(bias_docs[0].page_content)
+
+    ids = [str(i) for i in range(0, len(bias_docs))]
+    vectordb = Chroma.from_documents(bias_docs, embedding=embeddings, ids=ids)
+    
+
+
+    
+    
+
 
 
 
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
-from openai import AzureOpenAI
+# from openai import AzureOpenAI
 import os
 from dotenv import load_dotenv
+from langchain_openai import AzureOpenAI
 
 # Load environment variables from .env file
 
@@ -14,3 +15,5 @@ def get_client():
                         api_key=os.getenv("AZURE_OPENAI_API_KEY"),
 )
 
+if __name__ == "__main__":
+    client = get_client()
